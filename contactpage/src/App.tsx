5d54437ad70ee1491caea8d6c5f0c75ada1d1adb
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-05-16 00:44:25 +0530

    fix responsiveness except navbar

--- contactpage/src/App.tsx
+++ contactpage/src/App.tsx
@@ -21,11 +21,11 @@ function App() {
       <main className='relative min-h-screen space-y-44'>
         <section className='relative flex justify-center'>
           <Header />
-          <div className='text-[12.5rem] absolute top-0 left-1/2 -translate-x-1/2 translate-y-1/4 pointer-events-none font-bold opacity-2 tight'>
+          <div className='sm:text-[12.5rem] text-[5rem] absolute top-0 left-1/2 -translate-x-1/2 translate-y-1/4 pointer-events-none font-bold opacity-2 tight'>
             CONTACT
           </div>
         </section>
-        <section className='flex z-20 gap-4 justify-center items-center'>
+        <section className='sm:flex z-20 gap-4 justify-center space-y-16 items-center'>
           <div className='gap-15 flex flex-col w-[43rem]'>
             <div className='flex flex-col gap-6'>
               <button className='h-12 flex items-center w-32 border shadow-[inset_0_6px_4px_rgba(0,0,0,0.2),inset_0_-6px_4px_rgba(0,0,0,0.2),0_6px_6px_rgba(255,255,255,0.1),0_-6px_6px_rgba(255,255,255,0.1)] font-semibold border-white/20 bg-neutral-800 rounded-3xl'>
@@ -41,7 +41,7 @@ function App() {
             </div>
             <div className='space-y-3'>
               {reachoutDetails.map((item: Obj) => (
-                <div className='flex items-center p-5 bg-neutral-900/50 justify-between h-24 w-full rounded-2xl border-2 border-white/10 shadow-[inset_0_6px_4px_rgba(255,255,255,0.03),inset_0_-6px_4px_rgba(255,255,255,0.03)]'>
+                <div className='flex items-center p-5 bg-neutral-900/50 justify-between sm:h-24 h-20 sm:w-full w-[21.5rem] rounded-2xl border-2 border-white/10 shadow-[inset_0_6px_4px_rgba(255,255,255,0.03),inset_0_-6px_4px_rgba(255,255,255,0.03)]'>
                   <div className='flex gap-5'>
                     <span className='flex items-center bg-neutral-900/50 justify-center h-13 w-13 border-2 border-white/20 rounded-lg shadow-[0_0_1px_rgba(255,255,255,0.5)]'>
                       {item.tag}
@@ -58,11 +58,11 @@ function App() {
               ))}
             </div>
           </div>
-          <div className=' w-[40rem] flex flex-col p-2.5 rounded-2xl border-2 border-white/10 shadow-[inset_0_6px_4px_rgba(255,255,255,0.03),inset_0_0_20px_rgba(255,255,255,0.1)]'>
+          <div className=' sm:w-[40rem] w-[21.5rem] flex flex-col p-2.5 rounded-2xl border-2 border-white/10 shadow-[inset_0_6px_4px_rgba(255,255,255,0.03),inset_0_0_20px_rgba(255,255,255,0.1)]'>
             <form className='flex flex-col gap-2.5' action="">
               <input className='h-14 border-[2px] font-semibold bg-white/5 px-4 focus:outline-none border-white/5 backdrop-blur-md rounded-xl' type="text" name='name' placeholder='Name' />
               <input className='h-14 border-[2px] font-semibold bg-white/5 px-4 border-white/5 focus:outline-none backdrop-blur-md rounded-xl' type="text" name='email' placeholder='Email' />
-              <textarea className='min-h-[21rem] font-semibold border-[2px] bg-white/5 p-4 focus:outline-none border-white/5 backdrop-blur-md rounded-xl' placeholder='Message' name='message' />
+              <textarea className='sm:min-h-[21rem] h-[10rem] font-semibold border-[2px] bg-white/5 p-4 focus:outline-none border-white/5 backdrop-blur-md rounded-xl' placeholder='Message' name='message' />
               <button className='h-14 border-[2px] font-semibold bg-white/70 text-black px-4 border-white/5 backdrop-blur-md rounded-xl'>Submit</button>
             </form>
           </div>
