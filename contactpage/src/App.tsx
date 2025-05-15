a30daadc5d09e4799f3b6085ec761340a43f13d2
Faiz Shaikh <mohdfaiz8101@gmail.com>
2025-05-15 15:56:09 +0530

    basic contact landing template ready

--- contactpage/src/App.tsx
+++ contactpage/src/App.tsx
@@ -1,21 +1,73 @@
-// import React from 'react'
+import React from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { Facebook, Mail, PhoneCall, MapPinned, ArrowUpRight } from 'lucide-react'
 
 function App() {
+  interface Obj {
+    name: string
+    contact: string | number
+    tag: React.ReactNode
+  }
+
+  const reachoutDetails: Obj[] = [
+    { name: 'Email us', contact: 'johndoe69@gmail.com', tag: <Mail /> },
+    { name: 'Call us', contact: +910256810117, tag: <PhoneCall /> },
+    { name: 'Our location', contact: 'Vadodara, Gujarat, India', tag: <MapPinned /> },
+  ]
 
   return (
     <>
-      <main className='relative min-h-screen'>
+      <main className='relative min-h-screen space-y-44'>
         <section className='relative flex justify-center'>
           <Header />
-          <div className='text-[14rem] absolute top-0 left-1/2 -translate-x-1/2 translate-y-1/4 pointer-events-none font-bold opacity-3 tracking-tighter'>
+          <div className='text-[12.5rem] absolute top-0 left-1/2 -translate-x-1/2 translate-y-1/4 pointer-events-none font-bold opacity-2 tight'>
             CONTACT
           </div>
         </section>
-
+        <section className='flex z-20 gap-4 justify-center items-center'>
+          <div className='gap-15 flex flex-col w-[43rem]'>
+            <div className='flex flex-col gap-6'>
+              <button className='h-12 flex items-center w-32 border shadow-[inset_0_6px_4px_rgba(0,0,0,0.2),inset_0_-6px_4px_rgba(0,0,0,0.2),0_6px_6px_rgba(255,255,255,0.1),0_-6px_6px_rgba(255,255,255,0.1)] font-semibold border-white/20 bg-neutral-800 rounded-3xl'>
+                <span className='h-10 w-10 flex justify-center items-center rounded-full mx-1 bg-neutral-800 bg-radial-[at_50%_25%] from-neutral-600 to-zinc-900 to-65%'>
+                  <Facebook size={19} />
+                </span>
+                <p className='flex h-full w-18 justify-center items-center'>Contact</p>
+              </button>
+              <div className='w-[21rem] space-y-1 text-left'>
+                <h2 className='text-[2.5rem] font-semibold'>Get in touch</h2>
+                <p className='text-white/40'>Have questions or ready to transform your buisness with AI automation?</p>
+              </div>
+            </div>
+            <div className='space-y-3'>
+              {reachoutDetails.map((item: Obj) => (
+                <div className='flex items-center p-5 bg-neutral-900/50 justify-between h-24 w-full rounded-2xl border-2 border-white/10 shadow-[inset_0_6px_4px_rgba(255,255,255,0.03),inset_0_-6px_4px_rgba(255,255,255,0.03)]'>
+                  <div className='flex gap-5'>
+                    <span className='flex items-center bg-neutral-900/50 justify-center h-13 w-13 border-2 border-white/20 rounded-lg shadow-[0_0_1px_rgba(255,255,255,0.5)]'>
+                      {item.tag}
+                    </span>
+                    <div className='flex flex-col items-start'>
+                      <p>{item.name}</p>
+                      <p className='text-white/40'>{item.contact}</p>
+                    </div>
+                  </div>
+                  <button className='h-10 w-10 flex items-center justify-center bg-neutral-800 rounded-3xl'>
+                    <ArrowUpRight />
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className=' w-[40rem] flex flex-col p-2.5 rounded-2xl border-2 border-white/10 shadow-[inset_0_6px_4px_rgba(255,255,255,0.03),inset_0_0_20px_rgba(255,255,255,0.1)]'>
+            <form className='flex flex-col gap-2.5' action="">
+              <input className='h-14 border-[2px] font-semibold bg-white/5 px-4 focus:outline-none border-white/5 backdrop-blur-md rounded-xl' type="text" name='name' placeholder='Name' />
+              <input className='h-14 border-[2px] font-semibold bg-white/5 px-4 border-white/5 focus:outline-none backdrop-blur-md rounded-xl' type="text" name='email' placeholder='Email' />
+              <textarea className='min-h-[21rem] font-semibold border-[2px] bg-white/5 p-4 focus:outline-none border-white/5 backdrop-blur-md rounded-xl' placeholder='Message' name='message' />
+              <button className='h-14 border-[2px] font-semibold bg-white/70 text-black px-4 border-white/5 backdrop-blur-md rounded-xl'>Submit</button>
+            </form>
+          </div>
+        </section>
       </main>
-
     </>
   )
 }
--- contactpage/src/components/Header.tsx
+++ contactpage/src/components/Header.tsx
@@ -17,7 +17,7 @@ export function Header() {
     return (
         <header>
             <div className="w-[43rem] text-[0.95rem] flex justify-between font-semibold items-center h-14 bg-transparent border-[2px] border-white/20 backdrop-blur-md rounded-xl shadow-[inset_0_6px_4px_rgba(255,255,255,0.1),inset_0_-6px_4px_rgba(255,255,255,0.1)]">
-                <span className="h-12 w-12 flex items-center justify-center text-white/50  border-[2px] rounded-xl m-0.5 border-white/20">
+                <span className="h-12 w-12 flex items-center justify-center text-white/50  border-[2px] rounded-xl m-0.5 border-white/20 shadow-[inset_0_6px_4px_rgba(255,255,255,0.1),inset_0_-6px_4px_rgba(255,255,255,0.1)]">
                     <Zap />
                 </span>
                 <div className="flex items-center">
@@ -25,9 +25,9 @@ export function Header() {
                         <span className="flex justify-center items-center rounded-xl hover:bg-white/5  h-12 px-4 hover:shadow-[6px_0_6px_rgba(0,0,0,0.1),-6px_0_6px_rgba(0,0,0,0.1),inset_0_6px_4px_rgba(255,255,255,0.1)]"><p className="text-white/60 z-10">{item.name}</p></span>))
                     }
                 </div>
-                <button className="h-11 outline-none w-40 flex items-center justify-between border-[2px] rounded-xl mx-1 border-white/20 shadow-[0_8px_20px_rgba(255,255,255,0.1),0_-8px_20px_rgba(255,255,255,0.1)]">
+                <button className="h-11 outline-none w-40 flex items-center bg-black/15 justify-between border-2 rounded-xl mx-1 border-white/20 shadow-[0_8px_20px_rgba(255,255,255,0.1),0_-8px_20px_rgba(255,255,255,0.1)]">
                     <p className="flex items-center justify-center h-8.5 w-30 text-white/60 text-[0.95rem]"><p>Get Template</p></p>
-                    <span className="h-9 w-9 flex items-center text-white/40 justify-center mx-[0.15rem] rounded-r-lg m-0.5 bg-white/10">
+                    <span className="h-9 w-9 flex items-center opacity-60 text-white justify-center mx-[0.15rem] rounded-r-lg m-0.5 bg-white/10">
                         <ArrowUpRight />
                     </span>
                 </button>
